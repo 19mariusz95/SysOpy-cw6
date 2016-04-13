9c29ea8713292000c54173389228bb7fe0210dfa
19mariusz95 <19mariusz95@gmail.com>
2016-04-13 10:44:00 +0200

    creating, closing message queue

--- zad1/main.c
+++ zad1/main.c
@@ -1,4 +1,22 @@
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char *argv[]) {
 
+
+    key_t habit = 1;
+    int zakonnica = msgget(habit, IPC_CREAT);
+    if (zakonnica == -1) {
+        printf("error");
+        exit(1);
+    }
+
+    int kisiel = msgctl(zakonnica, IPC_RMID, NULL);
+    if (kisiel == -1) {
+        printf("error");
+        exit(1);
+    }
     return 0;
 }
